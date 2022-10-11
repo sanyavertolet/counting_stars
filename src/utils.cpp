6ddfae6d34a06fdc82a986162be05d89fdf1a39e
sanyavertolet <lxnfrolov@gmail.com>
2022-10-11 14:36:54 +0300

    [Fix] tests failed (x5)
    ### What's done:
     * Added default to switch-cases
     * Made explicit Pos constructor call when using to_string
    ### Todo:
    * Add more tests for Rational_number class
    * Add more tests for Complex_number class
    * Add more tests for Vector<TValue>
    * Implement bit matrices
    * Improve exceptions by adding more exceptions and more constructors for them (by analogy with FileNotFoundException)
    ### Passed:
     * Exceptions
     * Doxygen
     * StringInt
     * Rational_number
     * Complex_number
     * Vector<TValue != bool>
     * Tests

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,9 +5,18 @@
  */
 #include "utils.h"
 
+#define ____PROD____
+
+#ifdef ____PROD____
+std::filesystem::path project_path() {
+    return std::filesystem::current_path();
+}
+
+#else
 std::filesystem::path project_path() {
     return std::filesystem::current_path().parent_path();
 }
+#endif //____PROD____
 
 std::string get_uncommented_line(std::istream& is) {
     int ch;
